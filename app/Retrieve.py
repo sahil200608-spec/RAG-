71126fda15060283aa401581403680c47a537063
Sahil Khan <sahilkhan@Sahils-MacBook-Air.local>
2026-08-21 12:30:47 +0530

    Added vector database

--- app/Retrieve.py
+++ app/Retrieve.py
@@ -1,46 +1,31 @@
-from sentence_transformers import SentenceTransformer
-import json
-import numpy as np
+import chromadb
+from sentence_transformers import SentenceTransformer 
+
+client = chromadb.PersistentClient(
+    path ="data/chroma"
+)
+
+collection = client.get_collection(
+    name = "documents"
+)
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-with open("data/processed/embeddings.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-chunks = data["chunks"]
-
-
 query = "What is deliberate practice?"
 
+query_embedding = model.encode(query).tolist()
 
-query_embedding = model.encode(query)
+results = collection.query(
+    query_embeddings=[query_embedding],
+    n_results= 3
+)
 
-results=[]
+for i in range(len(results["documents"][0])):
 
-for chunk in chunks:
+    print("\n" + "-" * 80)
 
-    chunk_embedding = np.array(chunk["embedding"])
+    print("Pages:", results["metadatas"][0][i]["pages"])
 
-    similarity = np.dot(query_embedding, chunk_embedding)/(
-        np.linalg.norm(query_embedding)*
-        np.linalg.norm(chunk_embedding)
-    )
+    print("Distance:", results["distances"][0][i])
 
-    results.append({
-        "chunk": chunk,
-        "score": similarity
-    })
-
-results.sort(key = lambda x : x["score"] , reverse=True );
-
-top_k = 3
-
-for result in results[:top_k]:
-
-    print("Score:", result["score"])
-
-    print("Pages:", result["chunk"]["pages"])
-
-    print("Content:", result["chunk"]["content"])
-
-    print("-" * 80)
+    print("Content:", results["documents"][0][i])
